0ca3416eebd576854c2a10304400184f2e50f51b
SSRS21 <61640869+SSRS21@users.noreply.github.com>
2020-03-01 12:33:41 +0900

    Create Inverse.cpp

---
+++ Modulo/Inverse.cpp
@@ -0,0 +1,15 @@
+//Input: a, MOD
+//Output: inverse of a in mod MOD
+//Time: O(log MOD)
+long long modpow(long long a, long long b){
+	long long res = 1;
+	while (b > 0){
+		if (b % 2 == 1) res = res * a % MOD;
+		a = a * a % MOD;
+		b = b / 2;
+	}
+	return res;
+}
+long long modinv(long long a){
+	return modpow(a, MOD - 2);
+}
