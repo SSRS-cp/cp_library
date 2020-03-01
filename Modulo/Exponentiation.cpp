3f5e82be56535605c277cf992697ca31252325fa
SSRS21 <61640869+SSRS21@users.noreply.github.com>
2020-03-01 12:32:35 +0900

    Create Exponentiation.cpp

---
+++ Modulo/Exponentiation.cpp
@@ -0,0 +1,12 @@
+//Input: a, b, p
+//Output: (a^b) mod p
+//Order: O(log b)
+long long modpow(long long a, long long b){
+	long long res = 1;
+	while (b > 0){
+		if (b % 2 == 1) res = res * a % MOD;
+		a = a * a % MOD;
+		b = b / 2;
+	}
+	return res;
+}
