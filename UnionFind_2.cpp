84944df3bb892c588b098d62a3f8dbe74c0bce12
SSRS21 <61640869+SSRS21@users.noreply.github.com>
2020-03-01 14:29:23 +0900

    Create UnionFind_2.cpp

---
+++ UnionFind_2.cpp
@@ -0,0 +1,24 @@
+int unionfind_root(vector<int> &parent, int x){
+  if (parent[x] == x){
+    return x;
+  } else {
+    int y = unionfind_root(parent, parent[x]);
+    parent[x] = y;
+    return y;
+  }
+}
+void unionfind_union(vector<int> &parent, vector<int> &rank, int x, int y){
+  x = unionfind_root(parent, x);
+  y = unionfind_root(parent, y);
+  if (x != y){
+    if (rank[x] < rank[y]){
+      parent[x] = y;
+    } else if (rank[y] < rank[x]){
+      parent[y] = x;
+    } else {
+      parent[y] = x;
+      rank[x]++;
+    }
+  }
+  return;
+}
