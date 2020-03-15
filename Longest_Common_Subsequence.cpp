5d38de22e0b5e2945c6724e547bb16a297e88347
SSRS21 <61640869+SSRS21@users.noreply.github.com>
2020-03-16 00:48:39 +0900

    Update Longest_Common_Subsequence.cpp

--- Longest_Common_Subsequence.cpp
+++ Longest_Common_Subsequence.cpp
@@ -1,4 +1,4 @@
-int longest_common_subsequence_length(string S, string T){
+int longest_common_subsequence_length(string &S, string &T){
 	vector<vector<int>> dp(S.size() + 1, vector<int>(T.size() + 1, 0));
 	for (int i = 0; i < S.size(); i++){
 		for (int j = 0; j < T.size(); j++){
@@ -11,7 +11,7 @@ int longest_common_subsequence_length(string S, string T){
 	}
 	return dp[S.size()][T.size()];
 }
-string longest_common_subsequence(string S, string T){
+string longest_common_subsequence(string &S, string &T){
 	vector<vector<int>> dp(S.size() + 1, vector<int>(T.size() + 1, 0));
 	for (int i = 0; i < S.size(); i++){
 		for (int j = 0; j < T.size(); j++){
@@ -34,6 +34,47 @@ string longest_common_subsequence(string S, string T){
 			i--;
 			j--;
 			ans += S[i];
+		}
+	}
+	reverse(ans.begin(), ans.end());
+	return ans;
+}
+int longest_common_subsequence_length(vector<int> &A, vector<int> &B){
+	vector<vector<int>> dp(A.size() + 1, vector<int>(B.size() + 1, 0));
+	for (int i = 0; i < A.size(); i++){
+		for (int j = 0; j < B.size(); j++){
+			if (A[i] == B[j]){
+				dp[i + 1][j + 1] = dp[i][j] + 1;
+			} else {
+				dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1]);
+			}
+		}
+	}
+	return dp[A.size()][B.size()];
+}
+vector<int> longest_common_subsequence(vector<int> &A, vector<int> &B){
+	vector<vector<int>> dp(A.size() + 1, vector<int>(B.size() + 1, 0));
+	for (int i = 0; i < A.size(); i++){
+		for (int j = 0; j < B.size(); j++){
+			if (A[i] == B[j]){
+				dp[i + 1][j + 1] = dp[i][j] + 1;
+			} else {
+				dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1]);
+			}
+		}
+	}
+	vector<int> ans;
+	int i = A.size();
+	int j = B.size();
+	while (dp[i][j] > 0){
+		if (dp[i - 1][j] == dp[i][j]){
+			i--;
+		} else if (dp[i][j - 1] == dp[i][j]){
+			j--;
+		} else {
+			i--;
+			j--;
+			ans += A[i];
 		}
 	}
 	reverse(ans.begin(), ans.end());
