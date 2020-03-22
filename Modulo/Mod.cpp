f30bb962f4a6a04be8d9b68f9c2338bce19541cb
SSRS21 <61640869+SSRS21@users.noreply.github.com>
2020-03-22 15:04:16 +0900

    Create Mod.cpp

---
+++ Modulo/Mod.cpp
@@ -0,0 +1,48 @@
+long long modsub(long long a, long long b){
+	if (a >= b){
+		return (a - b) % MOD;
+	} else {
+		return p - (b - a) % MOD;
+	}
+}
+long long modpow(long long a, long long b){
+	long long res = 1;
+	while (b > 0){
+		if (b % 2 == 1) res = res * a % MOD;
+		a = a * a % MOD;
+		b = b / 2;
+	}
+	return res;
+}
+long long modinv(long long a){
+	return modpow(a, MOD - 2);
+}
+vector<long long> mf;
+long long modfact(long long n){
+	if (n < mf.size()){
+		return mf[n];
+	} else {
+		if (mf.empty()) mf.push_back(1);
+		long long res = mf.back();
+		for (int i = mf.size(); i <= n; i++){
+			res = res * i % MOD;
+			mf.push_back(res);
+		}
+		return res;
+	}
+}
+long long modbinom1(long long n, long long r){
+	long long res;
+	res = modfact(n);
+	res = res * modinv(modfact(r)) % MOD;
+	res = res * modinv(modfact(n - r)) % MOD ;
+	return res;
+}
+long long modbinom2(long long n, long long r){
+	long long res = 1;
+	for (int i = 0; i < r; i++){
+		res = res * (n - i) % MOD;
+		res = res * modinv(i + 1) % MOD;
+	}
+	return res;
+}
