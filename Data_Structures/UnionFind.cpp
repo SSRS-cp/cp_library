d1f60e912ecf6924467c54d87e24bc86ddc0d892
SSRS21 <61640869+SSRS21@users.noreply.github.com>
2020-03-22 17:32:32 +0900

    Update UnionFind.cpp

--- Data_Structures/UnionFind.cpp
+++ Data_Structures/UnionFind.cpp
@@ -7,11 +7,12 @@ int unionfind_root(vector<int> &parent, int x){
 		return y;
 	}
 }
-void unionfind_union(vector<int> &parent, int x, int y){
+bool unionfind_union(vector<int> &parent, int x, int y){
 	x = unionfind_root(parent, x);
 	y = unionfind_root(parent, y);
 	if (x != y){
 		parent[x] = y;
+		return false;
 	}
-	return;
+	return true;
 }
