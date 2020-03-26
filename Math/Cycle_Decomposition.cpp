7a063c865dff31a9f522932b0a8664500dd5367d
SSRS21 <61640869+SSRS21@users.noreply.github.com>
2020-03-26 15:54:22 +0900

    Create Cycle_Decomposition.cpp

---
+++ Math/Cycle_Decomposition.cpp
@@ -0,0 +1,16 @@
+vector<vector<int>> cycle_decomposition(vector<int> &P){
+	int N = P.size();
+	vector<bool> used(N, false);;
+	vector<vector<int>> ans;
+	for (int i = N - 1; i >= 0; i--){
+		if (used[i]) continue;
+		vector<int> C;
+		C.push_back(i);
+		while (C.front() != P[C.back()]){
+			C.push_back(P[C.back()]);
+			used[C.back()] = true;
+		}
+	ans.push_back(C);
+	}
+	return ans;
+}
