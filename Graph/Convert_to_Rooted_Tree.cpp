71c9d92dc6b067a4edb60f39d752306693654771
SSRS21 <61640869+SSRS21@users.noreply.github.com>
2020-03-29 16:19:17 +0900

    Create Convert_to_Rooted_Tree.cpp

---
+++ Graph/Convert_to_Rooted_Tree.cpp
@@ -0,0 +1,25 @@
+vector<int> parent(vector<vector<int>> &E){
+	int N = E.size();
+	vector<int> p(N, -1);
+	queue<int> Q;
+	Q.push(0);
+	while (!Q.empty()){
+		int v = Q.front();
+		Q.pop();
+		for (int w : E[v]){
+			if (p[w] == -1){
+				p[w] = v;
+				Q.push(w);
+			}
+		}
+	}
+	return p;
+}
+vector<vector<int>> child(vector<int> &p){
+	int N = p.size();
+	vector<vector<int>> c(N);
+	for (int i = 1; i < N; i++){
+		c[p[i]].push_back(i);
+	}
+	return c;
+}
