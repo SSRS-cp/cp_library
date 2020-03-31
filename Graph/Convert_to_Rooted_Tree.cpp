2a8e0ff8b0c4c504472651a53e1333806ead5925
SSRS21 <61640869+SSRS21@users.noreply.github.com>
2020-03-31 22:25:44 +0900

    Update Convert_to_Rooted_Tree.cpp

--- Graph/Convert_to_Rooted_Tree.cpp
+++ Graph/Convert_to_Rooted_Tree.cpp
@@ -15,7 +15,24 @@ vector<int> parent(vector<vector<int>> &E, int r){
 	}
 	return p;
 }
-vector<vector<int>> child(vector<int> &p, int r){
+vector<vector<int>> child(vector<vector<int>> E, int r){
+	int N = E.size();
+	vector<vector<int>> c(N);
+	queue<int> Q;
+	Q.push(r);
+	while (!Q.empty()){
+		int v = Q.front();
+		Q.pop();
+		for (int w : E[v]){
+			if (c[w].empty()){
+				c[v].push_back(w);
+				Q.push(w);
+			}
+		}
+	}
+	return c;
+}
+vector<vector<int>> child_2(vector<int> &p, int r){
 	int N = p.size();
 	vector<vector<int>> c(N);
 	for (int i = 0; i < N; i++){
