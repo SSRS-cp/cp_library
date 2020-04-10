8bc0ba8b846db7af403393e42896ec07e92aa38e
SSRS21 <61640869+SSRS21@users.noreply.github.com>
2020-04-11 02:12:10 +0900

    Update Prime.cpp

--- Math/Prime.cpp
+++ Math/Prime.cpp
@@ -1,14 +1,14 @@
 bool is_prime(long long N){
-	for (int i = 2; i * i <= N; i++){
+	for (long long i = 2; i * i <= N; i++){
 		if (N % i == 0){
 			return false;
 		}
 	}
 	return true;
 }
-vector<long long> prime_factorizarion(long long N){
+vector<long long> prime_factorization(long long N){
 	vector<long long> ans;
-	for (int i = 2; i * i <= N; i++){
+	for (long long i = 2; i * i <= N; i++){
 		if (N % i == 0){
 			while (N % i == 0){
 				N /= i;
@@ -18,11 +18,12 @@ vector<long long> prime_factorizarion(long long N){
 	}
 	if (N != 1){
 		ans.push_back(N);
+	}
 	return ans;
 }
 int factor_count(long long N){
 	int ans = 1;
-	for (int i = 2; i * i <= N; i++){
+	for (long long i = 2; i * i <= N; i++){
 		if (N % i == 0){
 			int e = 0;
 			while (N % i == 0){
