c2d26cfc42161c54ab2bedaac3977aeae458ec47
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2020-04-29 16:39:06 +0900

    Update Line.cpp
    projection, reflection, perpendicular_line, is_orthogonal, is_parallelを追加

--- Geometry/Line.cpp
+++ Geometry/Line.cpp
@@ -1,6 +1,11 @@
 struct line{
 	point A;
 	point B;
+	line(){
+	}
+	line(point P){
+	  B = P;
+	}
 	line(point P, point Q){
 		A = P;
 		B = Q;
@@ -18,9 +23,6 @@ struct line{
 			B = piint(1, -(b + c) / a);
 		}
 	}
-	point vec(){
-		return B - A;
-	}
 };
 istream& operator >>(istream &is, line &L){
 	point A, B;
@@ -31,4 +33,26 @@ istream& operator >>(istream &is, line &L){
 ostream& operator <<(ostream &os, line L){
 	os << L.A << ' ' << L.B;
 	return os;
+}
+point vec(line L){
+	return L.B - L.A;
+}
+point projection(point P, line L){
+	return L.A + vec(L) * dot(P - L.A, vec(L)) / norm(vec(L));
+}
+point reflection(point P, line L){
+	return projection(P, L) * 2 - P;
+}
+line perpendicular_line(point P, line L){
+	if (abs(ccw(L.A, L.B, P) == 1)){
+		return line(P, projection(point P, line L));
+	} else {
+		return line(P, P + rotate90(vec(L)));
+	}
+}
+bool is_orthogonal(line a, line b){
+	return sign(dot(vec(a), vec(b))) == 0;
+}
+bool is_parallel(line a, line b){
+  return sign(cross(vec(a), vec(b))) == 0;
 }
