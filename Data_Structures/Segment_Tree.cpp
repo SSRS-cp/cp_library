5cd7baf8351d7fb59a50849f27efb837a0b15a2d
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2020-05-09 12:55:19 +0900

    Update Segment_Tree.cpp
    use struct

--- Data_Structures/Segment_Tree.cpp
+++ Data_Structures/Segment_Tree.cpp
@@ -1,39 +1,56 @@
-vector<int> segtree(vector<int> &A){
-	int N = 1;
-	while (N < A.size()){
-		N = N * 2;
+template <typename T>
+struct segment_tree{
+	int N;
+	vector<T> ST;
+	function<T(T, T)> f;
+	T E;
+	segment_tree(int n, function<T(T, T)> F, T e){
+		N = 1;
+		while (N < n){
+			N *= 2;
+		}
+		ST = vector<T>(N * 2 - 1, e);
+		f = F;
+		E = e;
 	}
-	vector<int> ST(N * 2 - 1);
-	for (int i = 0; i < A.size(); i++){
-		ST[i + N - 1] = A[i];
+	segment_tree(vector<T> A, function<T(T, T)> F, T e){
+		int n = A.size();
+		N = 1;
+		while (N < n){
+			N *= 2;
+		}
+		ST = vector<T>(N * 2 - 1, e);
+		for (int i = 0; i < n; i++){
+			ST[N - 1 + i] = A[i];
+		}
+		for (int i = N - 2; i >= 0; i--){
+			ST[i] = F(ST[i * 2 + 1], ST[i * 2 + 2]);
+		}
+		f = F;
+		E = e;
 	}
-	for (int i = A.size() + N - 1; i < N * 2 - 1; i++){
-		ST[i] = INF;
+	T operator [](int k){
+		return ST[N - 1 + k];
 	}
-	for (int i = N - 2; i >= 0; i--){
-		ST[i] = min(ST[i * 2 + 1], ST[i * 2 + 2]);
+	void update(int k, T x){
+		k += N - 1;
+		ST[k] = x;
+		while (k > 0){
+			k = (k - 1) / 2;
+			ST[k] = f(ST[k * 2 + 1], ST[k * 2 + 2]);
+		}
 	}
-	return ST;
-}
-void segtree_update(vector<int> &ST, int i, int x){
-	int N = (ST.size() + 1) / 2;
-	i = i + N - 1;
-	ST[i] = x;
-	while (i > 0){
-		i = (i - 1) / 2;
-		ST[i] = min(ST[i * 2 + 1], ST[i * 2 + 2]);
+	T val(int L, int R, int i, int l, int r){
+		if (R <= l || r <= L){
+			return E;
+		} else if (L <= l && r <= R){
+			return ST[i];
+		} else {
+			int m = (l + r) / 2;
+			return f(val(L, R, i * 2 + 1, l, m), val(L, R, i * 2 + 2, m, r));
+		}
 	}
-	return;
-}
-int segtree_query(vector<int> &ST, int x, int y, int i, int L, int R){
-	if (y <= L || R <= x){
-		return INF;
-	} else if (x <= L && R <= y){
-		return ST[i];
-	} else {
-		int M = (L + R) / 2;
-		int A = segtree_query(ST, x, y, i * 2 + 1, L, M);
-		int B = segtree_query(ST, x, y, i * 2 + 2, M, R);
-		return min(A, B);
+	T query(int L, int R){
+		return val(L, R, 0, 0, N);
 	}
-}
+};
