037799af2f47e58100c1a505007e6ba3104d1bb9
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2020-05-09 15:53:51 +0900

    Create Point_Add_Range_Sum.cpp

---
+++ Range_Queries/Point_Add_Range_Sum.cpp
@@ -0,0 +1,33 @@
+template <typename T>
+struct point_add_range_sum{
+	int N;
+	vector<T> BIT;
+	point_add_range_sum(int n){
+		N = 1;
+		while (N < n){
+			N *= 2;
+		}
+		BIT = vector<T>(N + 1, 0);
+	}
+	void point_add(int i, T x){
+		i++;
+		while (i <= N){
+			BIT[i] += x;
+			i += i & -i;
+		}
+	}
+	T sum(int i){
+		T ans = 0;
+		while (i > 0){
+			ans += BIT[i];
+			i -= i & -i;
+		}
+		return ans;
+	}
+	T range_sum(int L, int R){
+		return sum(R) - sum(L);
+	}
+	T all_sum(){
+		return BIT[0];
+	}
+};
