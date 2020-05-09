26bd28e7f23558297077adee486cec8a7e7a2788
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2020-05-09 14:50:39 +0900

    Create Binary_Indexed_Tree.cpp

---
+++ Data_Structures/Binary_Indexed_Tree.cpp
@@ -0,0 +1,30 @@
+template <typename T>
+struct binary_indexed_tree{
+	int N;
+	vector<T> BIT;
+	binary_indexed_tree(int n){
+		N = 1;
+		while (N < n){
+			N *= 2;
+		}
+		BIT = vector<T>(N + 1, 0);
+	}
+	void add(int i, T x){
+		i++;
+		while (i <= N){
+			BIT[i] += x;
+			i += i & -i;
+		}
+	}
+	T sum(int i){
+		T ans = 0;
+		while (i > 0){
+			ans += BIT[i];
+			i -= i & -i;
+		}
+		return ans;
+	}
+	T query(int L, int R){
+		return sum(R) - sum(L);
+	}
+};
