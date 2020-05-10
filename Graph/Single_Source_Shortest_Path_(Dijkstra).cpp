ed30063152ede05abcc07cec0ce7de5d376a89d9
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2020-05-10 14:22:38 +0900

    Update Single_Source_Shortest_Path_(Dijkstra).cpp
    dijkstra_pathを追加

--- Graph/Single_Source_Shortest_Path_(Dijkstra).cpp
+++ Graph/Single_Source_Shortest_Path_(Dijkstra).cpp
@@ -1,18 +1,60 @@
 vector<long long> dijkstra(vector<vector<pair<int, int>>> &E, int s){
-	int V = E.size();
-	vector<long long> d(V, INF);
+	int N = E.size();
+	vector<long long> d(N, INF);
+	d[s] = 0;
 	priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> Q;
 	Q.push(make_pair(0, s));
 	while (!Q.empty()){
 		long long c = Q.top().first;
 		int v = Q.top().second;
 		Q.pop();
-		if (d[v] == INF){
-			d[v] = c;
+		if (d[v] >= c){
 			for (auto P : E[v]){
-				Q.push(make_pair(P.first + c, P.second));
+				int w = P.second;
+				if (d[w] > d[v] + P.first){
+					d[w] = d[v] + P.first;
+					Q.push(make_pair(d[w], w));
+				}
 			}
 		}
 	}
 	return d;
+}
+pair<long long, vector<int>> dijkstra_path(vector<vector<pair<int, int>>> &E, int s, int t){
+	int N = E.size();
+	vector<long long> d(N, INF);
+	d[s] = 0;
+	vector<int> prev(N);
+	priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> Q;
+	Q.push(make_pair(0, s));
+	while (!Q.empty()){
+		long long c = Q.top().first;
+		int v = Q.top().second;
+		Q.pop();
+		if (d[v] >= c){
+			for (auto P : E[v]){
+				int w = P.second;
+				if (d[w] > d[v] + P.first){
+					d[w] = d[v] + P.first;
+					Q.push(make_pair(d[w], w));
+					prev[w] = v;
+				}
+			}
+		}
+	}
+	if (d[t] == INF){
+		return make_pair(-1, vector<int>());
+	} else {
+		vector<int> path;
+		path.push_back(t);
+		while (1){
+			int v = path.back();
+			if (v == s){
+				break;
+			}
+			path.push_back(prev[v]);
+		}
+		reverse(path.begin(), path.end());
+		return make_pair(d[t], path);
+	}
 }
