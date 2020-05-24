ee07f3267edacd82e1bce2f59cf5ae79fff61a53
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2020-05-24 11:42:49 +0900

    Update Euler_Tour_Subtree_Query.cpp

--- Data_Structures/Euler_Tour_Subtree_Query.cpp
+++ Data_Structures/Euler_Tour_Subtree_Query.cpp
@@ -1,33 +1,26 @@
 template <typename T>
 struct euler_tour_subtree_query{
-	int N;
-	vector<vector<int>> c;
+	vector<T> A;
 	vector<int> left;
 	vector<int> right;
-	vector<T> value;
-	vector<T> A;
 	T E;
 	function<T(T, T)> f;
 	segment_tree<T> ST;
-	euler_tour_subtree_query(vector<vector<int>> &C, vector<T> &a, T e, function<T(T, T)> F){
-		c = C;
-		E = e;
-		f = F;
-		value = a;
-		N = c.size();
-		left = vector<int>(N);
-		right = vector<int>(N);
-	}
-	void dfs(int v){
+	void dfs(int v, vector<vector<int>> &c, vector<T> &a){
 		left[v] = A.size();
-		A.push_back(value[v]);
+		A.push_back(a[v]);
 		for (int w : c[v]){
-			dfs(w);
+			dfs(w, c, a);
 		}
 		right[v] = A.size();
 	}
-	void set(){
-		dfs(0);
+	euler_tour_subtree_query(vector<vector<int>> &c, vector<T> &a, T e, function<T(T, T)> F){
+		E = e;
+		f = F;
+		int N = c.size();
+		left = vector<int>(N);
+		right = vector<int>(N);
+		dfs(0, c, a);
 		ST = segment_tree<T>(A, E, f);
 	}
 	void update(int v, T x){
