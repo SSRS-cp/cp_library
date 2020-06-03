3d1b51dd6ccb96941b6148cc9a02f413b37f26f0
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2020-06-04 01:24:25 +0900

    Create Max_Flow_(Ford_Fulkerson).cpp

---
+++ Graph/Max_Flow_(Ford_Fulkerson).cpp
@@ -0,0 +1,52 @@
+struct ford_fulkerson{
+	vector<unordered_map<int, int>> E;
+	int V;
+	int F;
+	ford_fulkerson(vector<unordered_map<int, int>> &E): E(E), V(E.size()), F(0){
+	}
+	int max_flow(){
+		while (1){
+			vector<int> d(V, INF);
+			vector<int> m(V, INF);
+			vector<int> prev(V, -1);
+			d[0] = 0;
+			queue<int> Q;
+			Q.push(0);
+			while (!Q.empty()){
+				int v = Q.front();
+				Q.pop();
+				for (auto P : E[v]){
+					int w = P.first;
+					if (d[w] == INF){
+						prev[w] = v;
+						d[w] = d[v] + 1;
+						m[w] = min(m[v], P.second);
+						Q.push(w);
+					}
+				}
+			}
+			if (d[V - 1] == INF){
+				break;
+			}
+			int f = m[V - 1];
+			int c = V - 1;
+			while (1){
+				if (E[prev[c]][c] == f){
+					E[prev[c]].erase(c);
+				} else {
+					E[prev[c]][c] -= f;
+				}
+				E[c][prev[c]] += f;
+				c = prev[c];
+				if (c == 0){
+					break;
+				}
+			}
+			F += f;
+		}
+		return F;
+	}
+	void add_edge(int v, int w, int cap){
+		E[v][w] = cap;
+	}
+};
