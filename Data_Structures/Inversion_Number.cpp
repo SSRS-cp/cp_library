3c5da05830f70e132251de3ac119add4bd7d6590
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2020-06-06 01:12:17 +0900

    Update Inversion_Number.cpp

--- Data_Structures/Inversion_Number.cpp
+++ Data_Structures/Inversion_Number.cpp
@@ -17,3 +17,31 @@ long long inversion_number(vector<int> &p){
 	}
 	return ans;
 }
+long long inversion_number(vector<int> &A){
+	int N = A.size();
+	vector<int> B = A;
+	sort(B.begin(), B.end());
+	map<int, int> mp;
+	for (int i = 0; i < N; i++){
+		mp[B[i]] = i + 1;
+	}
+	for (int i = 0; i < N; i++){
+		A[i] = mp[A[i]];
+	}
+	long long ans = 0;
+	vector<int> BIT(N + 1, 0);
+	for (int i = 0; i < N; i++){
+		ans += i;
+		int j = A[i];
+		while (j > 0){
+			ans -= BIT[j];
+			j -= j & -j;
+		}
+		j = A[i];
+		while (j <= N){
+			BIT[j]++;
+			j += j & -j;
+		}
+	}
+	return ans;
+}
