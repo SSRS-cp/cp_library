f283981e36d715b4ac6f6a8e4a2c5d2714a1898e
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2020-06-06 11:16:26 +0900

    Create Topological_Sort.cpp

---
+++ Graph/Topological_Sort.cpp
@@ -0,0 +1,28 @@
+vector<int> topological_sort(vector<vector<int>> &E){
+	int N = E.size();
+	vector<int> deg(N, 0);
+	for (int i = 0; i < N; i++){
+		for (int j : E[i]){
+			deg[j]++;
+		}
+	}
+	queue<int> Q;
+	for (int i = 0; i < N; i++){
+		if (deg[i] == 0){
+			Q.push(i);
+		}
+	}
+	vector<int> ans;
+	while (!Q.empty()){
+		int v = Q.front();
+		Q.pop();
+		ans.push_back(v);
+		for (int w : E[v]){
+			deg[w]--;
+			if (deg[w] == 0){
+				Q.push(w);
+			}
+		}
+	}
+	return ans;
+}
