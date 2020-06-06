1bfc2e006978deab787fed58675f6b5ed341ceca
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2020-06-06 17:10:24 +0900

    Update Rerooting.cpp

--- Graph/Rerooting.cpp
+++ Graph/Rerooting.cpp
@@ -1,3 +1,43 @@
+template <typename T>
+struct rerooting{
+	vector<T> dp1;
+	vector<T> dp2;
+	T E;
+	function<T(T, T)> add;
+	function<T(T)> root;
+	void dfs1(vector<vector<int>> &c, int v){
+		dp1[v] = E;
+		for (auto w : c[v]){
+			dfs1(c, w);
+			dp1[v] = add(dp1[v], root(dp1[w]));
+		}
+	}
+	void dfs2(vector<vector<int>> &c, int v){
+		int deg = c[v].size();
+		vector<T> L(deg + 1, E);
+		for (int i = 0; i < deg; i++){
+			L[i + 1] = add(L[i], root(dp1[c[v][i]]));
+		}
+		vector<T> R(deg + 1, E);
+		for (int i = deg - 1; i >= 0; i--){
+			R[i] = add(R[i + 1], root(dp1[c[v][i]]));
+		}
+		for (int i = 0; i < deg; i++){
+			dp2[c[v][i]] = root(add(dp2[v], add(L[i], R[i + 1])));
+			dfs2(c, c[v][i]);
+		}
+	}
+	rerooting(vector<vector<int>> &c, function<T(T, T)> add, function<T(T)> root, T E): add(add), root(root), E(E){
+		int N = c.size();
+		dp1 = vector<T>(N, E);
+		dfs1(c, 0);
+		dp2 = vector<T>(N, E);
+		dfs2(c, 0);
+	}
+	T operator [](int k){
+		return add(dp1[k], dp2[k]);
+	}
+};
 struct rerooting{
 	vector<T> dp1;
 	vector<T> dp2;
@@ -35,6 +75,6 @@ struct rerooting{
 		dfs2(c, 0);
 	}
 	T operator [](int k){
-		return root(add(dp1[k], dp2[k]), 0);
+		return add(dp1[k], dp2[k]);
 	}
 };
