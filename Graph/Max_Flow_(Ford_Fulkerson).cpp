94677892b22631e34ab0b739b339536f930e04a9
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2020-06-06 12:54:15 +0900

    Update Max_Flow_(Ford_Fulkerson).cpp

--- Graph/Max_Flow_(Ford_Fulkerson).cpp
+++ Graph/Max_Flow_(Ford_Fulkerson).cpp
@@ -1,3 +1,44 @@
+int ford_fulkerson(vector<unordered_map<int, int>> &E, int s, int t){
+	int N = E.size();
+	int F = 0;
+	while (1){
+		vector<int> d(N, INF);
+		vector<int> m(N, INF);
+		vector<int> prev(N, -1);
+		d[s] = 0;
+		queue<int> Q;
+		Q.push(s);
+		while (!Q.empty()){
+			int v = Q.front();
+			Q.pop();
+			for (auto P : E[v]){
+				int w = P.first;
+				if (d[w] == INF){
+					prev[w] = v;
+					d[w] = d[v] + 1;
+					m[w] = min(m[v], P.second);
+					Q.push(w);
+				}
+			}
+		}
+		if (d[t] == INF){
+			break;
+		}
+		int f = m[N - 1];
+		int c = N - 1;
+		while (c != 0){
+			if (E[prev[c]][c] == f){
+				E[prev[c]].erase(c);
+			} else {
+				E[prev[c]][c] -= f;
+			}
+			E[c][prev[c]] += f;
+			c = prev[c];
+		}
+		F += f;
+	}
+	return F;
+}
 struct ford_fulkerson{
 	vector<unordered_map<int, int>> E;
 	int V;
