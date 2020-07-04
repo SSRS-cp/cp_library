7bc786b166ec15ad3d1a7113b70071b0c3ec708c
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2020-07-04 23:42:25 +0900

    Update Modulo.cpp

--- Math/Modulo.cpp
+++ Math/Modulo.cpp
@@ -3,48 +3,48 @@ long long modsub(long long a, long long b){
 	return (a % MOD - b % MOD + MOD) % MOD;
 }
 long long modpow(long long a, long long b){
-  long long ans = 1;
-  while (b > 0){
-    if (b % 2 == 1){
-      ans *= a;
-      ans %= MOD;
-    }
-    a *= a;
-    a %= MOD;
-    b /= 2;
-  }
-  return ans;
+	long long ans = 1;
+	while (b > 0){
+		if (b % 2 == 1){
+			ans *= a;
+			ans %= MOD;
+		}
+		a *= a;
+		a %= MOD;
+		b /= 2;
+	}
+	return ans;
 }
 long long modinv(long long a){
-  return modpow(a, MOD - 2);
+	return modpow(a, MOD - 2);
 }
 vector<long long> mf = {1};
 vector<long long> mfi = {1};
 long long modfact(int n){
-  if (mf.size() > n){
-    return mf[n];
-  } else {
-    for (int i = mf.size(); i <= n; i++){
-      long long next = mf.back() * i % MOD;
-      mf.push_back(next);
-      mfi.push_back(modinv(next));
-    }
-    return mf[n];
-  }
+	if (mf.size() > n){
+		return mf[n];
+	} else {
+		for (int i = mf.size(); i <= n; i++){
+			long long next = mf.back() * i % MOD;
+			mf.push_back(next);
+			mfi.push_back(modinv(next));
+		}
+		return mf[n];
+	}
 }
 long long modfactinv(int n){
-  if (mfi.size() > n){
-    return mfi[n];
-  } else {
-    return modinv(modfact(n));
-  }
+	if (mfi.size() > n){
+		return mfi[n];
+	} else {
+		return modinv(modfact(n));
+	}
 }
 long long modbinom(int n, int k){
-  if (n < 0 || k < 0 || k > n){
-    return 0;
-  } else {
-    return modfact(n) * modfactinv(k) % MOD * modfactinv(n - k) % MOD;
-  }
+	if (n < 0 || k < 0 || k > n){
+		return 0;
+	} else {
+		return modfact(n) * modfactinv(k) % MOD * modfactinv(n - k) % MOD;
+	}
 }
 long long modbinom2(long long n, int r){
 	long long ans = 1;
