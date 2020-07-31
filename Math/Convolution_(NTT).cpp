10c3d8a28df4d8d15b0f4a897fa3659fd60697c9
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2020-08-01 02:51:41 +0900

    Update Convolution_(NTT).cpp

--- Math/Convolution_(NTT).cpp
+++ Math/Convolution_(NTT).cpp
@@ -1,35 +1,23 @@
 vector<long long> ntt(vector<long long> A, bool inv){
 	int N = A.size();
-	int M = 0;
-	for (int i = 1; i < N; i *= 2){
-		M++;
+	long long r = 3;
+	if (inv){
+		r = modinv(r);
 	}
-	for (int i = 0; i < N; i++){
-		int j = 0;
-		for (int k = 0; k < M; k++){
-			if ((i >> k) & 1){
-				j |= 1 << (M - 1 - k);
-			}
-		}
-		if (i < j){
-			swap(A[i], A[j]);
-		}
-	}
-	for (int i = 1; i < N; i *= 2){
-		long long z = modpow(3, (MOD - 1) / (i * 2));
-		if (inv){
-			z = modinv(z);
-		}
+	vector<long long> B(N);
+	for (int i = N / 2; i > 0; i /= 2){
+		long long z = modpow(r, (MOD - 1) / (N / i));
 		long long z2 = 1;
-		for (int j = 0; j < i; j++){
-			for (int k = 0; k < N; k += i * 2){
-				long long s = A[j + k];
-				long long t = A[j + k + i] * z2 % MOD;
-				A[j + k] = (s + t) % MOD;
-				A[j + k + i] = (s - t + MOD) % MOD;;
+		for (int j = 0; j < N; j += i * 2){
+			for (int k = 0; k < i; k++){
+				A[i + j + k] *= z2;
+				A[i + j + k] %= MOD;
+				B[j / 2 + k] = (A[j + k] + A[i + j + k]) % MOD;
+				B[N / 2 + j / 2 + k] = (A[j + k] - A[i + j + k] + MOD) % MOD;
 			}
 			z2 = z2 * z % MOD;
 		}
+		swap(A, B);
 	}
 	if (inv){
 		int Ninv = modinv(N);
@@ -39,28 +27,24 @@ vector<long long> ntt(vector<long long> A, bool inv){
 	}
 	return A;
 }
-vector<long long> convolution(vector<long long> A, vector<long long> B){
+vector<long long> convolution(vector<long long> A, vector<long long> B, int d = -1){
 	int deg = A.size() + B.size() - 1;
 	int N = 1;
 	while (N < deg){
 		N *= 2;
 	}
-	while (A.size() < N){
-		A.push_back(0);
-	}
-	while (B.size() < N){
-		B.push_back(0);
-	}
-	vector<long long> a = ntt(A, false);
-	vector<long long> b = ntt(B, false);
-	vector<long long> c(N);
+	A.resize(N);
+	B.resize(N);
+	A = ntt(A, false);
+	B = ntt(B, false);
+	vector<long long> ans(N);
 	for (int i = 0; i < N; i++){
-		c[i] = a[i] * b[i] % MOD;
+		ans[i] = A[i] * B[i] % MOD;
 	}
-	c = ntt(c, true);
-	vector<long long> ans(deg);
-	for (int i = 0; i < deg; i++){
-		ans[i] = c[i];
+	ans = ntt(ans, true);
+	if (d != -1){
+		deg = d;
 	}
+	ans.resize(d);
 	return ans;
 }
