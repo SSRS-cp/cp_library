3400c361c94060283ea040a299745b5f4f1a3326
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2020-09-22 22:27:07 +0900

    Update Strongly_Connected_Component_Decomposition.cpp

--- Graph/Strongly_Connected_Component_Decomposition.cpp
+++ Graph/Strongly_Connected_Component_Decomposition.cpp
@@ -1,53 +1,60 @@
 struct strongly_connected_components{
-	vector<vector<int>> E1;
-	vector<vector<int>> E2;
-	vector<int> t;
-	vector<bool> used;
-	vector<bool> used2;
 	vector<vector<int>> ans;
-	void dfs1(int v){
-		for (int w : E1[v]){
+	vector<int> scc;
+	void dfs1(vector<vector<int>> &E, vector<int> &t, vector<bool> &used, int v){
+		for (int w : E[v]){
 			if (!used[w]){
 				used[w] = true;
-				dfs1(w);
+				dfs1(E, t, used, w);
 			}
 		}
 		t.push_back(v);
 	}
-	void dfs2(int v){
+	void dfs2(vector<vector<int>> &E2, vector<bool> &used2, int v){
 		ans.back().push_back(v);
 		for (int w : E2[v]){
 			if (!used2[w]){
 				used2[w] = true;
-				dfs2(w);
+				dfs2(E2, used2, w);
 			}
 		}
 	}
-	strongly_connected_components(vector<vector<int>> &G){
-		int V = G.size();
-		E1 = vector<vector<int>>(V);
-		E2 = vector<vector<int>>(V);
-		used = vector<bool>(V, false);
-		used2 = vector<bool>(V, false);
-		for (int i = 0; i < V; i++){
-			for (int j : G[i]){
-				E1[i].push_back(j);
+	strongly_connected_components(vector<vector<int>> &E){
+		int N = E.size();
+		vector<vector<int>> E2(N);
+		for (int i = 0; i < N; i++){
+			for (int j : E[i]){
 				E2[j].push_back(i);
 			}
 		}
-		for (int i = 0; i < V; i++){
+		vector<int> t;
+		vector<bool> used(N, false);
+		for (int i = 0; i < N; i++){
 			if (!used[i]){
 				used[i] = true;
-				dfs1(i);
+				dfs1(E, t, used, i);
 			}
 		}
 		reverse(t.begin(), t.end());
-		for (int i = 0; i < V; i++){
+		vector<bool> used2(N, false);
+		scc = vector<int>(N);
+		int cnt = 0;
+		for (int i = 0; i < N; i++){
 			if (!used2[t[i]]){
 				used2[t[i]] = true;
 				ans.push_back(vector<int>());
-				dfs2(t[i]);
+				dfs2(E2, used2, t[i]);
+				for (int j : ans.back()){
+					scc[j] = cnt;
+				}
+				cnt++;
 			}
 		}
+	}
+	int operator [](int k){
+		return scc[k];
+	}
+	int size(){
+		return ans.size();
 	}
 };
