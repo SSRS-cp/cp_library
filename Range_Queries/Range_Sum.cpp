c8538412a795984b2477798243305010ec422123
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2020-10-08 22:35:43 +0900

    Update Range_Sum.cpp

--- Range_Queries/Range_Sum.cpp
+++ Range_Queries/Range_Sum.cpp
@@ -1,15 +1,16 @@
+//Range Sum
 template <typename T>
-struct range_sum{
+struct prefix_sum{
 	int N;
 	vector<T> S;
-	range_sum(vector<T> A){
+	prefix_sum(vector<T> A){
 		N = A.size();
 		S = vector<T>(N + 1, 0);
 		for (int i = 0; i < N; i++){
 			S[i + 1] = S[i] + A[i];
 		}
 	}
-	T query(int p, int q){
+	T range_sum(int p, int q){
 		return S[q] - S[p];
 	}
 	T all(){
