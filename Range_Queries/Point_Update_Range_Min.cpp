1bb97d58adf4440b7b39cbfcfa6ebe78f0c7239e
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2020-10-08 22:30:01 +0900

    Update Point_Update_Range_Min.cpp

--- Range_Queries/Point_Update_Range_Min.cpp
+++ Range_Queries/Point_Update_Range_Min.cpp
@@ -1,15 +1,16 @@
+//point update range min
 template <typename T>
-struct point_update_range_min{
+struct segment_tree{
 	int N;
 	vector<T> ST;
-	point_update_range_min(int n){
+	segment_tree(int n){
 		N = 1;
 		while (N < n){
 			N *= 2;
 		}
 		ST = vector<T>(N * 2 - 1, INF);
 	}
-	point_update_range_min(vector<T> A){
+	segment_tree(vector<T> A){
 		int n = A.size();
 		N = 1;
 		while (N < n){
@@ -26,7 +27,7 @@ struct point_update_range_min{
 	T operator [](int k){
 		return ST[N - 1 + k];
 	}
-	void point_update(int k, T x){
+	void update(int k, T x){
 		k += N - 1;
 		ST[k] = x;
 		while (k > 0){
@@ -34,20 +35,20 @@ struct point_update_range_min{
 			ST[k] = min(ST[k * 2 + 1], ST[k * 2 + 2]);
 		}
 	}
-	T val(int L, int R, int i, int l, int r){
+	T range_min(int L, int R, int i, int l, int r){
 		if (R <= l || r <= L){
 			return INF;
 		} else if (L <= l && r <= R){
 			return ST[i];
 		} else {
 			int m = (l + r) / 2;
-			return min(val(L, R, i * 2 + 1, l, m), val(L, R, i * 2 + 2, m, r));
+			return min(range_min(L, R, i * 2 + 1, l, m), range_min(L, R, i * 2 + 2, m, r));
 		}
 	}
 	T range_min(int L, int R){
-		return val(L, R, 0, 0, N);
+		return range_min(L, R, 0, 0, N);
 	}
-	T all_min(){
+	T all(){
 		return ST[0];
 	}
 };
