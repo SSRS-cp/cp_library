86566db1a085b639da136b397fe26e6a7399b4ee
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2021-01-03 02:01:20 +0900

    Update XOR_Convolution.cpp

--- Math/XOR_Convolution.cpp
+++ Math/XOR_Convolution.cpp
@@ -1,27 +1,31 @@
-vector<long long> fwt(vector<long long> A, int N, bool inv){
-	for (int i = 0; i < N; i++){
-		for (int j = 0; j < (1 << N); j++){
-			if (!(j >> i & 1)){
+vector<long long> xor_fwt(vector<long long> A, bool inv){
+	int N = A.size();
+	for (int i = 1; i < N; i <<= 1){
+		for (int j = 0; j < N; j++){
+			if ((j & i) == 0){
 				long long x = A[j];
-				long long y = A[j | (1 << i)];
-				A[j] = x + y;
-				A[j | (1 << i)] = x - y;
+				long long y = A[j | i];
+				A[j] = (x + y) % MOD;
+				A[j | i] = (x + MOD - y) % MOD;
 				if (inv){
-					A[j] /= 2;
-					A[j | (1 << i)] /= 2;
+					A[j] *= (MOD + 1) / 2;
+					A[j] %= MOD;
+					A[j | i] *= (MOD + 1) / 2;
+					A[j | i] %= MOD;
 				}
 			}
 		}
 	}
 	return A;
 }
-vector<long long> xor_convolution(vector<long long> A, vector<long long> B, int N){
-	A = fwt(A, N, false);
-	B = fwt(B, N, false);
-	vector<long long> C(1 << N);
-	for (int i = 0; i < (1 << N); i++){
-	C[i] = A[i] * B[i];
+vector<long long> xor_convolution(vector<long long> A, vector<long long> B){
+	int N = A.size();
+	A = xor_fwt(A, false);
+	B = xor_fwt(B, false);
+	vector<long long> C(N);
+	for (int i = 0; i < N; i++){
+		C[i] = A[i] * B[i] % MOD;
 	}
-	C = fwt(C, N, true);
+	C = xor_fwt(C, true);
 	return C;
 }
