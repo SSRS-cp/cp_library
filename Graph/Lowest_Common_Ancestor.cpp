bbb23a4d98234166d8a20aac442283473f3598f5
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2021-01-23 13:48:46 +0900

    Update Lowest_Common_Ancestor.cpp

--- Graph/Lowest_Common_Ancestor.cpp
+++ Graph/Lowest_Common_Ancestor.cpp
@@ -1,49 +1,45 @@
 struct lowest_common_ancestor{
-	vector<int> d;
-	vector<vector<int>> p;
-	lowest_common_ancestor(vector<int> &P, vector<vector<int>> &C){
-		int N = P.size();
-		d = vector<int>(N, 0);
-		queue<int> Q;
-		Q.push(0);
-		while (!Q.empty()){
-			int v = Q.front();
-			Q.pop();
-			for (int w : C[v]){
-				d[w] = d[v] + 1;
-				Q.push(w);
-			}
-		}
-		p = vector<vector<int>>(N, vector<int>(LOG, -1));
-		for (int i = 0; i < N; i++){
-			p[i][0] = P[i];
-		}
-		for (int i = 1; i < LOG; i++){
-			for (int j = 0; j < N; j++){
-				if (p[j][i - 1] != -1){
-					p[j][i] = p[p[j][i - 1]][i - 1];
-				}
-			}
-		}
-	}
-	int query(int u, int v){
-		if (d[u] > d[v]){
-			swap(u, v);
-		}
-		for (int k = 0; k < LOG; k++){
-			if ((d[v] - d[u]) >> k & 1){
-				v = p[v][k];
-			}
-		}
-		if (u == v){
-		    return u;
-		}
-		for (int k = LOG - 1; k >= 0; k--){
-			if (p[u][k] != p[v][k]){
-				u = p[u][k];
-				v = p[v][k];
-			}
-		}
-		return p[u][0];
-	}
+    vector<int> p, sz, in, next;
+    void dfs1(vector<vector<int>> &c, int v = 0){
+        sz[v] = 1;
+        for (int &w : c[v]){
+            dfs1(c, w);
+            sz[v] += sz[w];
+            if (sz[w] > sz[c[v][0]]){
+                swap(w, c[v][0]);
+            }
+        }
+    }
+    void dfs2(vector<vector<int>> &c, int &t, int v = 0){
+        in[v] = t;
+        t++;
+        for (int w : c[v]){
+            if (w == c[v][0]){
+                next[w] = next[v];
+            } else {
+                next[w] = w;
+            }
+            dfs2(c, t, w);
+        }
+    }
+    lowest_common_ancestor(vector<int> &p, vector<vector<int>> &c): p(p){
+        int N = p.size();
+        sz = vector<int>(N);
+        dfs1(c);
+        in = vector<int>(N);
+        next = vector<int>(N, 0);
+        int t = 0;
+        dfs2(c, t);
+    }
+    int lca(int u, int v){
+        while (1){
+            if (in[u] > in[v]){
+                swap(u, v);
+            }
+            if (next[u] == next[v]){
+                return u;
+            }
+            v = p[next[v]];
+        }
+    }
 };
