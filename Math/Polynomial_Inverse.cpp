57caa4048a32d045f9bbf6b5e04f639b8196a102
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2021-08-29 18:39:02 +0900

    Update Polynomial_Inverse.cpp

--- Math/Polynomial_Inverse.cpp
+++ Math/Polynomial_Inverse.cpp
@@ -1,1 +1,23 @@
-
+vector<long long> polynomial_inverse(vector<long long> f){
+  int N = f.size();
+  vector<long long> ans(1);
+  ans[0] = modinv(f[0]);
+  for (int i = 1; i < N * 2; i *= 2){
+    vector<long long> ans2 = ans;
+    ans2.resize(i * 4);
+    int N2 = min(N, i * 2);
+    vector<long long> f2(i * 4, 0);
+    for (int j = 0; j < N2; j++){
+      f2[j] = f[j];
+    }
+    ans2 = convolution(ans2, ans2, i * 2);
+    ans2 = convolution(ans2, f2, i * 2);
+    for (int j = 0; j < i; j++){
+      ans2[j] = MOD - ans2[j] + ans[j] * 2;
+      ans2[j] %= MOD;
+    }
+    swap(ans, ans2);
+  }
+  ans.resize(N);
+  return ans;
+}
