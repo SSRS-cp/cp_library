2167e48fb153d814d60f8f13b050adf03cafc8b0
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2021-09-07 15:02:25 +0900

    Update Binary_Indexed_Tree.cpp

--- Data_Structures/Binary_Indexed_Tree.cpp
+++ Data_Structures/Binary_Indexed_Tree.cpp
@@ -1,33 +1,37 @@
 template <typename T>
 struct binary_indexed_tree{
-	int N;
-	vector<T> BIT;
-	binary_indexed_tree(int n){
-		N = 1;
-		while (N < n){
-			N *= 2;
-		}
-		BIT = vector<T>(N + 1, 0);
-	}
-	void add(int i, T x){
-		i++;
-		while (i <= N){
-			BIT[i] += x;
-			i += i & -i;
-		}
-	}
-	T sum(int i){
-		T ans = 0;
-		while (i > 0){
-			ans += BIT[i];
-			i -= i & -i;
-		}
-		return ans;
-	}
-	T query(int L, int R){
-		return sum(R) - sum(L);
-	}
-	T all(){
-		return BIT[0];
-	}
+  int N;
+  vector<T> BIT;
+  binary_indexed_tree(){
+  }
+  binary_indexed_tree(int N): N(N), BIT(N + 1, 0){
+  }
+  binary_indexed_tree(vector<T> &A){
+    N = A.size();
+    BIT = vector<T>(N + 1, 0);
+    for (int i = 0; i < N; i++){
+      BIT[i + 1] = A[i];
+    }
+    for (int i = 1; i < N; i++){
+      BIT[i + (i & -i)] += BIT[i];
+    }
+  }
+  void add(int i, T x){
+    i++;
+    while (i <= N){
+      BIT[i] += x;
+      i += i & -i;
+    }
+  }
+  T sum(int i){
+    T ans = 0;
+    while (i > 0){
+      ans += BIT[i];
+      i -= i & -i;
+    }
+    return ans;
+  }
+  T sum(int L, int R){
+    return sum(R) - sum(L);
+  }
 };
