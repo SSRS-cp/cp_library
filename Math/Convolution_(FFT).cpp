bd2168ee33b04809fdacd981e6b7b7d98d2ef7f8
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2021-09-08 13:47:42 +0900

    Update Convolution_(FFT).cpp

--- Math/Convolution_(FFT).cpp
+++ Math/Convolution_(FFT).cpp
@@ -1,30 +1,23 @@
+double PI = acos(-1);
 vector<complex<double>> fft(vector<complex<double>> A, bool inv){
 	int N = A.size();
-	int M = 0;
-	for (int i = 1; i < N; i *= 2){
-		M++;
+	complex<double> r = polar((double) 1, (2 * PI) / (N));
+	if (inv){
+		r = pow(r, -1);
 	}
-	for (int i = 0; i < N; i++){
-		int j = 0;
-		for (int k = 0; k < M; k++){
-			if ((i >> k) & 1){
-				j |= 1 << (M - 1 - k);
+	vector<complex<double>> B(N);
+	for (int i = N / 2; i > 0; i /= 2){
+		complex<double> z = pow(r, i);
+		complex<double> z2 = 1;
+		for (int j = 0; j < N; j += i * 2){
+			for (int k = 0; k < i; k++){
+				A[i + j + k] *= z2;
+				B[j / 2 + k] = A[j + k] + A[i + j + k];
+				B[N / 2 + j / 2 + k] = A[j + k] - A[i + j + k];
 			}
+			z2 *= z;
 		}
-		if (i < j){
-			swap(A[i], A[j]);
-		}
-	}
-	for (int i = 1; i < N; i *= 2){
-		for (int j = 0; j < i; j++){
-			complex<double> z = polar((double) 1, (2 * PI) / (i * 2) * j * (inv ? -1 : 1));
-			for (int k = 0; k < N; k += i * 2){
-				complex<double> s = A[j + k];
-				complex<double> t = A[j + k + i] * z;
-				A[j + k] = s + t;
-				A[j + k + i] = s - t;
-			}
-		}
+		swap(A, B);
 	}
 	if (inv){
 		for (int i = 0; i < N; i++){
@@ -39,30 +32,24 @@ vector<double> convolution(vector<double> A, vector<double> B){
 	while (N < deg){
 		N *= 2;
 	}
-	vector<complex<double>> A2(N);
+	vector<complex<double>> A2(N, 0);
 	for (int i = 0; i < A.size(); i++){
 		A2[i] = A[i];
 	}
-	while (A2.size() < N){
-		A2.push_back(0);
-	}
-	vector<complex<double>> B2(N);
+	vector<complex<double>> B2(N, 0);
 	for (int i = 0; i < B.size(); i++){
 		B2[i] = B[i];
 	}
-	while (B2.size() < N){
-		B2.push_back(0);
-	}
-	vector<complex<double>> a = fft(A2, false);
-	vector<complex<double>> b = fft(B2, false);
-	vector<complex<double>> c(N);
+	A2 = fft(A2, false);
+	B2 = fft(B2, false);
+	vector<complex<double>> C2(N);
 	for (int i = 0; i < N; i++){
-		c[i] = a[i] * b[i];
+		C2[i] = A2[i] * B2[i];
 	}
-	c = fft(c, true);
-	vector<double> ans(deg);
+	C2 = fft(C2, true);
+	vector<double> C(deg);
 	for (int i = 0; i < deg; i++){
-		ans[i] = c[i].real();
+	  C[i] = C2[i].real();
 	}
-	return ans;
+	return C;
 }
