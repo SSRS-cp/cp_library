be1e82954bc76971e60e8484cc9a1f71f332e987
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2021-10-17 05:40:27 +0900

    Update Point.cpp

--- Geometry/Point.cpp
+++ Geometry/Point.cpp
@@ -1,153 +1,37 @@
-double EPS = 0.0000000001;
-int sign(double x){
-	if (x > EPS){
-		return 1;
-	} else if (x < -EPS){
-		return -1;
-	} else {
-		return 0;
-	}
-}
-bool equals(double x, double y){
-	return abs(x - y) < EPS;
-}
 struct point{
-	double x;
-	double y;
-	point(){
-		x = 0;
-		y = 0;
-	}
-	point(double X, double Y){
-		x = X;
-		y = Y;
-	}
-	point operator -(){
-		return point(-x, -y);
-	}
-	point operator +(point P){
-		return point(x + P.x, y + P.y);
-	}
-	point operator -(point P){
-		return point(x - P.x, y - P.y);
-	}
-	point operator *(double k){
-		return point(x * k, y * k);
-	}
-	point operator /(double k){
-		return point(x / k, y / k);
-	}
-	point& operator +=(point P){
-		x += P.x;
-		y += P.y;
-		return *this;
-	}
-	point& operator -=(point P){
-		x -= P.x;
-		y -= P.y;
-		return *this;
-	}
-	point& operator *=(double k){
-		x *= k;
-		y *= k;
-		return *this;
-	}
-	point& operator /=(double k){
-		x /= k;
-		y /= k;
-		return *this;
-	}
-	bool operator ==(point P){
-		return equals(x, P.x) && equals(y, P.y);
-	}
-	bool operator !=(point P){
-		return !(*this == P);
-	}
-	bool operator <(point P){
-		return sign(P.x - x) == 1 || equals(x, P.x) && sign(P.y - y) == 1;
-	}
-	bool operator >(point P){
-		return (P < *this);
-	}
-	point& rotate(double t){
-	    double X = x * cos(t) - y * sin(t);
-	    double Y = x * sin(t) + y * cos(t);
-	    x = X;
-	    y = Y;
-	    return *this;
-	}
-	point& rotate(point P, double t){
-	    *this -= P;
-	    (*this).rotate(t);
-	    *this += P;
-	    return *this;
-	}
-	point& rotate90(){
-	    swap(x, y);
-	    x = -x;
-	    return *this;
-	}
+  double x, y;
+  point(){
+  }
+  point(double x, double y): x(x), y(y){
+  }
+  point operator +(point P){
+    return point(x + P.x, y + P.y);
+  }
+  point operator -(point P){
+    return point(x - P.x, y - P.y);
+  }
+  point operator *(double k){
+    return point(x * k, y * k);
+  }
+  point operator /(double k){
+    return point(x / k, y / k);
+  }
 };
-istream& operator >>(istream &is, point &p){
-	double x, y;
-	is >> x >> y;
-	p = point(x, y);
-	return is;
-}
-ostream& operator <<(ostream &os, point p){
-	os << p.x << ' ' << p.y;
-	return os;
-}
-double norm(point P){
-	return P.x * P.x + P.y * P.y;
-}
-double abs(point P){
-	return sqrt(norm(P)); 
+point rotate90(point P){
+  return point(-P.y, P.x);
 }
 point rotate(point P, double t){
-	return point(P.x * cos(t) - P.y * sin(t), P.x * sin(t) + P.y * cos(t));
+  return point(P.x * cos(t) - P.y * sin(t), P.x * sin(t) + P.y * cos(t));
 }
-point rotate(point P, point Q, double t){
-	return rotate(P - Q, t) + Q;
-}
-point rotate90(point P){
-	return point(-P.y, P.x);
-}
-point midpoint(point P, point Q){
-	return (P + Q) / 2;
-}
-double dot(point P, point Q){
-	return P.x * Q.x + P.y * Q.y;
-}
-double cross(point P, point Q){
-	return P.x * Q.y - P.y * Q.x;
+double abs(point P){
+  return sqrt(P.x * P.x + P.y * P.y);
 }
 double dist(point P, point Q){
-	return abs(P - Q);
+  return abs(Q - P);
 }
-double arg(point P){
-	return atan2(P.y, P.x);
+double dot(point P, point Q){
+  return P.x * Q.x + P.y * Q.y;
 }
-int ccw(point P, point Q, point R){
-	Q -= P;
-	R -= P;
-	if (sign(cross(Q, R)) == 1){
-		return 1;
-	} else if (sign(cross(Q, R)) == -1){
-		return -1;
-	} else if (sign(dot(Q, R)) == -1){
-		return -2;
-	} else if (sign(abs(R) - abs(Q)) == 1){
-		return 2;
-	} else {
-		return 0;
-	}
-}
-double angle(point A, point B, point C){
-	A -= B;
-	C -= B;
-	return acos(dot(A, C) / (abs(A) * abs(C)));
-}
-int angle_type(point A, point B, point C){
-	return sign(dot(A - B, C - B));
+double cross(point P, point Q){
+  return P.x * Q.y - P.y * Q.x;
 }
