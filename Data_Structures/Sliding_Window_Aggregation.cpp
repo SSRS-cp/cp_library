c0d9c5850c56447ac435423217cd8f06c080e223
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2021-12-01 17:42:35 +0900

    Create Sliding_Window_Aggregation.cpp

---
+++ Data_Structures/Sliding_Window_Aggregation.cpp
@@ -0,0 +1,40 @@
+template <typename T>
+struct swag{
+  function<T(T, T)> f;
+  T E;
+  stack<pair<T, T>> fr, bk;
+  swag(function<T(T, T)> f, T E): f(f), E(E){
+  }
+  void push(T x){
+    if (fr.empty()){
+      fr.push(make_pair(x, x));
+    } else {
+      fr.push(make_pair(x, f(fr.top().second, x)));
+    }
+  }
+  void pop(){
+    if (bk.empty()){
+      while (!fr.empty()){
+        T x = fr.top().first;
+        fr.pop();
+        if (bk.empty()){
+          bk.push(make_pair(x, x));
+        } else {
+          bk.push(make_pair(x, f(x, bk.top().second)));
+        }
+      }
+    }
+    bk.pop();
+  }
+  T get(){
+    T ans1 = E;
+    if (!fr.empty()){
+      ans1 = fr.top().second;
+    }
+    T ans2 = E;
+    if (!bk.empty()){
+      ans2 = bk.top().second;
+    }
+    return f(ans2, ans1);
+  }
+};
