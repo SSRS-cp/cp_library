98b6f991f43f09186083976ed0d349ca2fe57665
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2021-12-01 17:46:59 +0900

    Update Z-algorithm.cpp

--- String/Z-algorithm.cpp
+++ String/Z-algorithm.cpp
@@ -1,18 +1,18 @@
 vector<int> z_algorithm(string S){
-	int N = S.size();
-	vector<int> ans(N);
-	for (int i = 1, j = 0; i < N; i++){
-		if (i + ans[i - j] < j + ans[j]){
-			ans[i] = ans[i - j];
-		} else {
-			int k = max(0, j + ans[j] - i);
-			while (i + k < N && S[k] == S[i + k]){
-				k++;
-			}
-			ans[i] = k;
-			j = i;
-		}
-	}
-	ans[0] = N;
-	return ans;
+  int N = S.size();
+  vector<int> ans(N);
+  for (int i = 1, j = 0; i < N; i++){
+    if (i + ans[i - j] < j + ans[j]){
+      ans[i] = ans[i - j];
+    } else {
+      int k = max(0, j + ans[j] - i);
+      while (i + k < N && S[k] == S[i + k]){
+        k++;
+      }
+      ans[i] = k;
+      j = i;
+    }
+  }
+  ans[0] = N;
+  return ans;
 }
