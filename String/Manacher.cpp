3764e5f2fe730b64acc93b3eb547d87b1f9c1326
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2021-12-01 17:44:57 +0900

    Create Manacher.cpp

---
+++ String/Manacher.cpp
@@ -0,0 +1,19 @@
+vector<int> manacher(string &S){
+  int N = S.size();
+  vector<int> ans(N, 0);
+  int i = 0, j = 0;
+  while (i < N){
+    while (i >= j && i + j < N && S[i - j] == S[i + j]){
+      j++;
+    }
+    ans[i] = j;
+    int k = 1;
+    while (i >= k && i + k < N && k + ans[i - k] < j){
+      ans[i + k] = ans[i - k];
+      k++;
+    }
+    i += k;
+    j -= k;
+  }
+  return ans;
+}
