b96e9e6c4b575cc9b477f9de2e6e63b1053cd514
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-05-13 12:19:52 +0900

    Update Two_Edge_Connected_Components.cpp
    https://judge.yosupo.jp/submission/89225

--- Graph/Two_Edge_Connected_Components.cpp
+++ Graph/Two_Edge_Connected_Components.cpp
@@ -1,62 +1,62 @@
 struct two_edge_connected_components{
-	vector<bool> used;
-	vector<int> ord, low, tcc;
-	int t, cnt;
-	void dfs1(vector<vector<int>> &E, int v, int p){
-		used[v] = true;
-		ord[v] = t;
-		low[v] = t;
-		t++;
-		bool p_used = false;
-		for (int w : E[v]){
-			if (p == w && !p_used){
-				p_used = true;
-				continue;
-			}
-			if (!used[w]){
-				dfs1(E, w, v);
-				low[v] = min(low[v], low[w]);
-			} else {
-				low[v] = min(low[v], ord[w]);
-			}
-		}
-	}
-	void dfs2(vector<vector<int>> &E, int v, int p){
-		if (p >= 0 && ord[p] >= low[v]){
-			tcc[v] = tcc[p];
-		} else {
-			tcc[v] = cnt;
-			cnt++;
-		}
-		for (int w : E[v]){
-			if (tcc[w] == -1){
-				dfs2(E, w, v);
-			}
-		}
-	}
-	two_edge_connected_components(vector<vector<int>> &E){
-		int N = E.size();
-		used = vector<bool>(N, false);
-		ord = vector<int>(N, 0);
-		low = vector<int>(N, 0);
-		t = 0;
-		for (int i = 0; i < N; i++){
-			if (!used[i]){
-				dfs1(E, i, -1);
-			}
-		}
-		tcc = vector<int>(N, -1);
-		cnt = 0;
-		for (int i = 0; i < N; i++){
-			if (tcc[i] == -1){
-				dfs2(E, i, -1);
-			}
-		}
-	}
-	int operator [](int k){
-		return tcc[k];
-	}
-	int size(){
-		return cnt;
-	}
+  vector<int> tcc;
+  int cnt;
+  two_edge_connected_components(vector<vector<int>> &E){
+    int N = E.size();
+    vector<int> p(N, -1);
+    vector<int> d(N, -1);
+    vector<int> imos(N, 0);
+    for (int i = 0; i < N; i++){
+      if (p[i] == -1){
+        d[i] = 0;
+        dfs1(E, p, d, imos, i);
+      }
+    }
+    tcc = vector<int>(N, -1);
+    cnt = 0;
+    for (int i = 0; i < N; i++){
+      if (tcc[i] == -1){
+        tcc[i] = cnt;
+        cnt++;
+        dfs2(E, p, imos, i);
+      }
+    }
+  }
+  void dfs1(vector<vector<int>> &E, vector<int> &p, vector<int> &d, vector<int> &imos, int v = 0){
+    bool pe = false;
+    for (int w : E[v]){
+      if (w != p[v] || pe){
+      	if (d[w] == -1){
+          p[w] = v;
+          d[w] = d[v] + 1;
+          dfs1(E, p, d, imos, w);
+          imos[v] += imos[w];
+        } else if (d[w] < d[v]){
+          imos[v]++;
+          imos[w]--;
+        }
+      } else {
+        pe = true;
+      }
+    }
+  }
+  void dfs2(vector<vector<int>> &E, vector<int> &p, vector<int> &imos, int v = 0){
+    for (int w : E[v]){
+      if (tcc[w] == -1){
+        if (imos[w] > 0){
+          tcc[w] = tcc[v];
+        } else {
+          tcc[w] = cnt;
+          cnt++;
+        }
+        dfs2(E, p, imos, w);
+      }
+    }
+  }
+  int operator [](int v){
+    return tcc[v];
+  }
+  int count(){
+    return cnt;
+  }
 };
