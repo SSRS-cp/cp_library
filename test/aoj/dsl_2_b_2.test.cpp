385208f42054d7a0e0b706fefe3c3a9cc4b9cc52
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-13 08:16:15 +0900

    Create dsl_2_b_2.test.cpp

---
+++ test/aoj/dsl_2_b_2.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_B"
+#include <bits/stdc++.h>
+using namespace std;
+#include "../../data_structure/sequence/invertible_binary_indexed_tree.cpp"
+int main(){
+  int n, q;
+  cin >> n >> q;
+  invertible_binary_indexed_tree<int> BIT(n, plus<int>(), negate<int>, 0);
+  for (int i = 0; i < q; i++){
+    int com, x, y;
+    cin >> com >> x >> y;
+    if (com == 0){
+      x--;
+      BIT.add(x, y);
+    }
+    if (com == 1){
+      x--;
+      cout << BIT.sum(x, y) << endl;
+    }
+  }
+}
