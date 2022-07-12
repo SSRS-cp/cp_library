9a44bb8922346b4d3aab5d5be8bcd9a723d21772
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-13 04:55:50 +0900

    Create static_rmq.test.cpp

---
+++ test/library_checker/static_rmq.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_rmq"
+#include <bits/stdc++.h>
+using namespace std;
+#include "../../data_structure/sequence/disjoint_sparse_table.cpp"
+int main(){
+  int N, Q;
+  cin >> N >> Q;
+  vector<int> a(N);
+  for (int i = 0; i < N; i++){
+    cin >> a[i];
+  }
+  disjoint_sparse_table<int> DST(a, [](int a, int b){return min(a, b);});
+  for (int i = 0; i < Q; i++){
+    int l, r;
+    cin >> l >> r;
+    cout << DST.query(l, r) << endl;
+  }
+}
