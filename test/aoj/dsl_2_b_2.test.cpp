e84557cdc1a167f203ea2ea1ba3dbc994ac3f87e
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-13 08:19:09 +0900

    Update dsl_2_b_2.test.cpp

--- test/aoj/dsl_2_b_2.test.cpp
+++ test/aoj/dsl_2_b_2.test.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
   int n, q;
   cin >> n >> q;
-  invertible_binary_indexed_tree<int> BIT(n, plus<int>(), negate<int>, 0);
+  invertible_binary_indexed_tree<int> BIT(n, plus<int>(), negate<int>(), 0);
   for (int i = 0; i < q; i++){
     int com, x, y;
     cin >> com >> x >> y;
