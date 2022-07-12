188319447c48b1f619f03629fc02f9815a250077
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-13 04:11:45 +0900

    Create abstract_binary_indexed_tree.cpp

---
+++ data_structure/sequence/abstract_binary_indexed_tree.cpp
@@ -0,0 +1,34 @@
+template <typename T>
+struct binary_indexed_tree{
+  int N;
+  vector<T> BIT;
+  function<T(T, T)> f;
+  T E;
+  binary_indexed_tree(function<T(T, T)> f, T E): f(f), E(E){
+  }
+  binary_indexed_tree(int N, function<T(T, T)> f, T E): f(f), E(E), N(N), BIT(N + 1, 0){
+  }
+  binary_indexed_tree(vector<T> &A, function<T(T, T)> f, T E): f(f), E(E), N(A.size()), BIT(N + 1){
+    for (int i = 0; i < N; i++){
+      BIT[i + 1] = A[i];
+    }
+    for (int i = 1; i < N; i++){
+      BIT[i + (i & -i)] = f(BIT[i + (i & -i)], BIT[i]);
+    }
+  }
+  void add(int i, T x){
+    i++;
+    while (i <= N){
+      BIT[i] = f(BIT[i], x);
+      i += i & -i;
+    }
+  }
+  T sum(int i){
+    T ans = E;
+    while (i > 0){
+      ans = f(ans, BIT[i]);
+      i -= i & -i;
+    }
+    return ans;
+  }
+};
