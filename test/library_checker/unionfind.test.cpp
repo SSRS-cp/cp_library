e1d380209ae45b58fd7d7d5d7136a62c579f3f49
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-13 02:24:02 +0900

    Update and rename unionfind.test.cpp to test/library_checker/unionfind.test.cpp

--- test/library_checker/unionfind.test.cpp
+++ test/library_checker/unionfind.test.cpp
@@ -1,34 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
 #include <bits/stdc++.h>
+#include "../../data_structure/unionfind.cpp"
 using namespace std;
-struct unionfind{
-	vector<int> p;
-	unionfind(int N){
-		p = vector<int>(N, -1);
-	}
-	int root(int x){
-		if (p[x] < 0){
-			return x;
-		} else {
-			p[x] = root(p[x]);
-			return p[x];
-		}
-	}
-	bool same(int x, int y){
-		return root(x) == root(y);
-	}
-	void unite(int x, int y){
-		x = root(x);
-		y = root(y);
-		if (x != y){
-			if (p[x] < p[y]){
-				swap(x, y);
-			}
-			p[y] += p[x];
-			p[x] = y;
-		}
-	}
-};
 int main(){
   int N, Q;
   cin >> N >> Q;
