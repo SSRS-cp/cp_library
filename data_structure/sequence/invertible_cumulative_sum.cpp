a34b2ec50cda3b1cd5f6e1b474045b471c8c19f9
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-13 04:32:10 +0900

    Create invertible_cumulative_sum.cpp

---
+++ data_structure/sequence/invertible_cumulative_sum.cpp
@@ -0,0 +1,23 @@
+template <typename T>
+struct cumulative_sum{
+  vector<T> S;
+  function<T(T, T)> f;
+  function<T(T)> inv;
+  T E;
+  cumulative_sum(){
+  }
+  cumulative_sum(vector<T> A, function<T(T, T)> f, function<T(T)> inv, T E): f(f), inv(inv), E(E){
+    int N = A.size();
+    S = vector<T>(N + 1);
+    S[0] = E;
+    for (int i = 0; i < N; i++){
+      S[i + 1] = f(S[i], A[i]);
+    }
+  }
+  T get(int i){
+    return S[i];
+  }
+  T get(int l, int r){
+    return f(S[r], inv(S[l]));
+  }
+};
