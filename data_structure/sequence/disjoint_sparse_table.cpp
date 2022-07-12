02cea297f6e59333a7f5d5709ac4a782c5b2e719
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-13 04:35:43 +0900

    Create disjoint_sparse_table.cpp

---
+++ data_structure/sequence/disjoint_sparse_table.cpp
@@ -0,0 +1,35 @@
+template <typename T>
+struct disjoint_sparse_table{
+  vector<T> A;
+  vector<vector<T>> D;
+  function<T(T, T)> f;
+  disjoint_sparse_table(){
+  }
+  disjoint_sparse_table(vector<T> &A, function<T(T, T)> f): A(A), f(f){
+    int N = A.size();
+    int LOG = 32 - __builtin_clz(N - 1);
+    D = vector<vector<T>>(LOG, vector<T>(N));
+    for (int i = 0; i < LOG; i++){
+      int d = 1 << i;
+      for (int j = 0; j + d < N; j += d * 2){
+        D[i][j + d - 1] = A[j + d - 1];
+        for (int k = j + d - 2; k >= j; k--){
+          D[i][k] = f(A[k], D[i][k + 1]);
+        }
+        D[i][j + d] = A[j + d];
+        for (int k = j + d + 1; k < min(j + d * 2, N); k++){
+          D[i][k] = f(D[i][k - 1], A[k]);
+        }
+      }
+    }
+  }
+  T query(int L, int R){
+    if (R - L == 1){
+      return A[L];
+    } else {
+      R--;
+      int b = 31 - __builtin_clz(R ^ L);
+      return f(D[b][L], D[b][R]);
+    }
+  }
+};
