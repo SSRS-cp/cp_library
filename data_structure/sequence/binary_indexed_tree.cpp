813991730462b0d0882f3253d7e5a0c4f7bf7be3
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-13 08:11:57 +0900

    Update binary_indexed_tree.cpp

--- data_structure/sequence/binary_indexed_tree.cpp
+++ data_structure/sequence/binary_indexed_tree.cpp
@@ -6,9 +6,9 @@ struct binary_indexed_tree{
   T E;
   binary_indexed_tree(){
   }
-  binary_indexed_tree(int N, function<T(T, T)> f, T E): f(f), E(E), N(N), BIT(N + 1, 0){
+  binary_indexed_tree(int N, function<T(T, T)> f, T E): N(N), BIT(N + 1, E), f(f), E(E){
   }
-  binary_indexed_tree(vector<T> &A, function<T(T, T)> f, T E): f(f), E(E), N(A.size()), BIT(N + 1){
+  binary_indexed_tree(vector<T> &A, function<T(T, T)> f, T E): N(A.size()), BIT(N + 1), f(f), E(E){
     for (int i = 0; i < N; i++){
       BIT[i + 1] = A[i];
     }
