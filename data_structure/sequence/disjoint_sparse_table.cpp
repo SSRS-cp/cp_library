7d9031119d14733f6a89ce8be933942f72680ed5
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-13 13:34:48 +0900

    Update disjoint_sparse_table.cpp

--- data_structure/sequence/disjoint_sparse_table.cpp
+++ data_structure/sequence/disjoint_sparse_table.cpp
@@ -6,23 +6,28 @@ struct disjoint_sparse_table{
   T E;
   disjoint_sparse_table(){
   }
+  disjoint_sparse_table(int N, function<T(T, T)> f, T E): A(N, E), f(f), E(E){
+    if (N > 1){
+      int LOG = 32 - __builtin_clz(N - 1);
+      D = vector<vector<T>>(LOG, vector<T>(N));
+    }
+  }
   disjoint_sparse_table(vector<T> &A, function<T(T, T)> f, T E): A(A), f(f), E(E){
     int N = A.size();
-    if (N == 1){
-      return;
-    }
-    int LOG = 32 - __builtin_clz(N - 1);
-    D = vector<vector<T>>(LOG, vector<T>(N));
-    for (int i = 0; i < LOG; i++){
-      int d = 1 << i;
-      for (int j = 0; j + d < N; j += d * 2){
-        D[i][j + d - 1] = A[j + d - 1];
-        for (int k = j + d - 2; k >= j; k--){
-          D[i][k] = f(A[k], D[i][k + 1]);
-        }
-        D[i][j + d] = A[j + d];
-        for (int k = j + d + 1; k < min(j + d * 2, N); k++){
-          D[i][k] = f(D[i][k - 1], A[k]);
+    if (N > 1){
+      int LOG = 32 - __builtin_clz(N - 1);
+      D = vector<vector<T>>(LOG, vector<T>(N));
+      for (int i = 0; i < LOG; i++){
+        int d = 1 << i;
+        for (int j = 0; j + d < N; j += d * 2){
+          D[i][j + d - 1] = A[j + d - 1];
+          for (int k = j + d - 2; k >= j; k--){
+            D[i][k] = f(A[k], D[i][k + 1]);
+          }
+          D[i][j + d] = A[j + d];
+          for (int k = j + d + 1; k < min(j + d * 2, N); k++){
+            D[i][k] = f(D[i][k - 1], A[k]);
+          }
         }
       }
     }
