df3ec2ba96c3cc59bc5431df413f4196cae7614e
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-14 01:12:04 +0900

    Create linear.cpp

---
+++ other/monoids/linear.cpp
@@ -0,0 +1,15 @@
+struct linear{
+  long long a, b;
+  linear(){
+    a = 1;
+    b = 0;
+  }
+  linear(int a, int b): a(a), b(b){
+  }
+};
+linear composite(linear A, linear B){
+  return linear(A.a * B.a % MOD, (A.b * B.a + B.b) % MOD);
+}
+int value(linear A, int x){
+  return (A.a * x + A.b) % MOD;
+}
