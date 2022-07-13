ff78404e07dddda9277ff1f46ee8e319e02f16e2
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-13 10:13:07 +0900

    Create static_rmq_2.test.cpp

---
+++ test/library_checker/static_rmq_2.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+#include <bits/stdc++.h>
+using namespace std;
+const int INF = 1000000000;
+#include "../../data_structure/sequence/sparse_table.cpp"
+int main(){
+  int N, Q;
+  cin >> N >> Q;
+  vector<int> a(N);
+  for (int i = 0; i < N; i++){
+    cin >> a[i];
+  }
+  sparse_table<int> DST(a, [](int a, int b){return min(a, b);}, INF);
+  for (int i = 0; i < Q; i++){
+    int l, r;
+    cin >> l >> r;
+    cout << DST.query(l, r) << endl;
+  }
+}
