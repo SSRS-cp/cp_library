6e3fb8994a5b5c4c23505bc90446107f1305cbe0
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-13 20:49:46 +0900

    Create dsl_2_b_3.test.cpp

---
+++ test/aoj/dsl_2_b_3.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_A"
+#include <bits/stdc++.h>
+using namespace std;
+#include "../../data_structure/sequence/segment_tree.cpp"
+int main(){
+  int n, q;
+  cin >> n >> q;
+  vector<int> a(n);
+  segment_tree<int> ST(n, plus<int>(), 0);
+  for (int i = 0; i < q; i++){
+    int com, x, y;
+    cin >> com >> x >> y;
+    if (com == 0){
+      x--;
+      a[x] += y;
+      ST.update(x, a[x]);
+    }
+    if (com == 1){
+      x--;
+      cout << ST.query(x, y) << endl;
+    }
+  }
+}
