5e6b0ba5356fb4ea73205595fc2d04e5edb6f072
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-14 02:03:46 +0900

    Create range_affine_point_get.test.cpp

---
+++ test/library_checker/range_affine_point_get.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_affine_point_get"
+#include <bits/stdc++.h>
+using namespace std;
+const long long MOD = 998244353;
+#include "../../data_structures/sequence/dual_segment_tree"
+#include "../../other/monoids/linear.cpp"
+int main(){
+  int N, Q;
+  cin >> N >> Q;
+  vector<linear> a(N);
+  for (int i = 0; i < N; i++){
+    a[i].a = 0;
+    cin >> a[i].b;
+  }
+  dual_segment_tree<linear> ST(a, composite, linear());
+  for (int i = 0; i < Q; i++){
+    int t;
+    cin >> t;
+    if (t == 0){
+      int l, r, b, c;
+      cin >> l >> r >> b >> c;
+      ST.range_apply(l, r, linear(b, c));
+    }
+    if (t == 1){
+      int p;
+      cin >> p;
+      cout << ST[p].b << endl;
+    }
+  }
+}
