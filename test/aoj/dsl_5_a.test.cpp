e82224ba3601412ff37591c8b07be9c0aad9931a
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-13 14:41:15 +0900

    Update dsl_5_a.test.cpp

--- test/aoj/dsl_5_a.test.cpp
+++ test/aoj/dsl_5_a.test.cpp
@@ -1,14 +1,16 @@
 #define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_5_A"
 #include <bits/stdc++.h>
 using namespace std;
-#include "../../data_structure/sequence/dual_invertible_binary_indexed_tree.cpp"
+#include "../../data_structure/sequence/dual_binary_indexed_tree.cpp"
 int main(){
   int N, T;
   cin >> N >> T;
-  dual_invertible_binary_indexed_tree<int> BIT(T, plus<int>(), negate<int>(), 0);
+  dual_binary_indexed_tree<int> BIT(T, plus<int>(), 0);
   for (int i = 0; i < N; i++){
     int l, r;
     cin >> l >> r;
+    BIT.add(l, -1);
+    BIT.add(r, 1);
     BIT.add(l, r, 1);
   }
   vector<int> S = BIT.get();
