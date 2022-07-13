4c582c0f46cdaa778ed17d5ee84abe2eec762b9a
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-14 01:30:39 +0900

    Create dsl_2_e_3.test.cpp

---
+++ test/aoj/dsl_2_e_3.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_E"
+#include <bits/stdc++.h>
+using namespace std;
+#include "../../data_structures/sequence/commutative_dual_segment_tree"
+int main(){
+  int n, q;
+  cin >> n >> q;
+  commutative_dual_segment_tree<int> ST(n, plus<int>(), 0);
+  for (int i = 0; i < q; i++){
+    int c;
+    cin >> c;
+    if (c == 0){
+      int s, t, x;
+      cin >> s >> t >> x;
+      s--;
+      ST.range_apply(s, t, x);
+    }
+    if (c == 1){
+      int t;
+      cin >> t;
+      t--;
+      cout << ST[t] << endl;
+    }
+  }
+}
