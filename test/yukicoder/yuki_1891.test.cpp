38591977f955035511f5fbf1a7671c33ea35f1a0
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-14 02:20:20 +0900

    Create yuki_1891.test.cpp

---
+++ test/yukicoder/yuki_1891.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1891"
+#include <bits/stdc++.h>
+using namespace std;
+const long long MOD = 998244353;
+#include "../../data_structure/sequence/xor_segment_tree.cpp"
+#include "../../other/monoids/linear.cpp"
+int main(){
+  int N, Q;
+  cin >> N >> Q;
+  vector<linear> f(N);
+  for (int i = 0; i < N; i++){
+    int a, b;
+    cin >> a >> b;
+    f[i] = linear(a, b);
+  }
+  xor_segment_tree<linear> F(f, composite, linear());
+  for (int i = 0; i < Q; i++){
+    int l, r, p, x;
+    cin >> l >> r >> p >> x;
+    cout << value(F.range_fold(l, r, p), x) << endl;
+  }
+}
