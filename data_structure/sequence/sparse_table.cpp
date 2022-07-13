301468b3a2d06044df67f95c1b2d140b3e69c4b9
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-13 10:11:52 +0900

    Create sparse_table.cpp

---
+++ data_structure/sequence/sparse_table.cpp
@@ -0,0 +1,26 @@
+template <typename T>
+struct sparse_table{
+  vector<vector<T>> ST;
+  function<T(T, T)> f;
+  T E;
+  sparse_table(vector<T> &A, function<T(T, T)> f, T E): f(f), E(E){
+    int N = A.size();
+    int LOG = 32 - __builtin_clz(N);
+    ST = vector<vector<int>>(LOG, vector<int>(N));
+    for (int i = 0; i < N; i++){
+      ST[0][i] = A[i];
+    }
+    for (int i = 0; i < LOG - 1; i++){
+      for (int j = 0; j < N - (1 << i); j++){
+        ST[i + 1][j] = f(ST[i][j], ST[i][j + (1 << i)]);
+      }
+    }
+  }
+  int range_min(int L, int R){
+    if (L == R){
+      return E;
+    }
+    int d = 31 - __builtin_clz(R - L);
+    return f(ST[d][L], ST[d][R - (1 << d)]);
+  }
+};
