f9a2a81557ce9c733f1b7554689adb8e08e533b4
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-14 01:15:13 +0900

    Create point_set_range_composite.test.cpp

---
+++ test/library_checker/point_set_range_composite.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_set_range_composite"
+#include <bits/stdc++.h>
+using namespace std;
+const long long MOD = 998244353;
+#include "../../data_structure/sequence/segment_tree.cpp"
+#include "../../other/monoids/linear.cpp"
+int main(){
+  int N, Q;
+  cin >> N >> Q;
+  vector<linear> f(N);
+  for (int i = 0; i < N; i++){
+    cin >> f[i].a >> f[i].b;
+  }
+  segment_tree<linear> ST(f, composite, linear());
+  for (int i = 0; i < Q; i++){
+    int t;
+    cin >> t;
+    if (t == 0){
+      int p, c, d;
+      cin >> p >> c >> d;
+      ST.update(p, linear(c, d));
+    }
+    if (t == 1){
+      int l, r, x;
+      cin >> l >> r >> x;
+      cout << value(ST.query(l, r), x) << endl;
+    }
+  }
+}
