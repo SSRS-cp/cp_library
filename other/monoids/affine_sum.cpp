e2afc21998fe3650ffdb776fc59b9305da35f7ce
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-14 02:36:26 +0900

    Create affine_sum.cpp

---
+++ other/monoids/affine_sum.cpp
@@ -0,0 +1,16 @@
+struct affine_sum{
+  int cnt;
+  long long sum;
+  affine_sum(): cnt(0), sum(0){
+  }
+};
+affine_sum op(affine_sum A, affine_sum B){
+  A.cnt += B.cnt;
+  A.sum += B.sum;
+  A.sum %= MOD;
+  return A;
+}
+affine_sum mp(linear f, affine_sum A){
+  A.sum = (A.sum * f.a + A.cnt * f.b) % MOD;
+  return A;
+}
