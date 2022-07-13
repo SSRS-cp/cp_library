1eff007257427f13d8411035272d73c188b66b7f
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-13 20:16:55 +0900

    Create point_add_range_sum_3.test.cpp

---
+++ test/library_checker/point_add_range_sum_3.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+#include <bits/stdc++.h>
+using namespace std;
+#include "../../data_structure/sequence/segment_tree.cpp"
+int main(){
+  int N, Q;
+  cin >> N >> Q;
+  vector<long long> a(N);
+  for (int i = 0; i < N; i++){
+    cin >> a[i];
+  }
+  segment_tree<long long> BIT(a, plus<long long>(), 0);
+  for (int i = 0; i < Q; i++){
+    int t;
+    cin >> t;
+    if (t == 0){
+      int p, x;
+      cin >> p >> x;
+      a[p] += x;
+      ST.update(p, a[p]);
+    }
+    if (t == 1){
+      int l, r;
+      cin >> l >> r;
+      cout << ST.query(l, r) << endl;
+    }
+  }
+}
