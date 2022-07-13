164fd804b497ecbea781721f5d6549e26fcafbf5
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-13 16:41:07 +0900

    Create yuki_945_1.test.cpp

---
+++ test/yukicoder/yuki_945_1.test.cpp
@@ -0,0 +1,33 @@
+#define PROBLEM "https://yukicoder.me/problems/no/945"
+#include <bits/stdc++.h>
+using namespace std;
+const int INF = 1000000;
+#include "../../data_structure/sequence/dual_disjoint_sparse_table.cpp"
+int main(){
+  int N, M;
+  cin >> N >> M;
+  dual_sparse_table<pair<int, int>> ST(N, [](pair<int, int> a, pair<int, int> b){return min(a, b);}, make_pair(INF, 0));
+  for (int i = 0; i < M; i++){
+    int L, R;
+    char T;
+    cin >> L >> R >> T;
+    L--;
+    if (T == 'Y'){
+      ST.apply(L, R, make_pair(i, 0));
+    }
+    if (T == 'K'){
+      ST.apply(L, R, make_pair(i, 1));
+    }
+    if (T == 'C'){
+      ST.apply(L, R, make_pair(i, 2));
+    }
+  }
+  vector<pair<int, int>> A = ST.get();
+  vector<int> ans(3, 0);
+  for (int i = 0; i < N; i++){
+    if (A[i].first != INF){
+      ans[A[i].second]++;
+    }
+  }
+  cout << ans[0] << ' ' << ans[1] << ' ' << ans[2] << endl;
+}
