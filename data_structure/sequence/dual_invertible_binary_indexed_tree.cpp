76ff76ca561d47310b600009b93c799a782eb39c
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-13 14:19:46 +0900

    Update dual_invertible_binary_indexed_tree.cpp

--- data_structure/sequence/dual_invertible_binary_indexed_tree.cpp
+++ data_structure/sequence/dual_invertible_binary_indexed_tree.cpp
@@ -1,15 +1,15 @@
 template <typename T>
-struct dual_invertible_binary_indexed_tree.cpp{
+struct dual_invertible_binary_indexed_tree{
   int N;
   vector<T> BIT;
   function<T(T, T)> f;
   function<T(T)> inv;
   T E;
-  dual_invertible_binary_indexed_tree.cpp(){
+  dual_invertible_binary_indexed_tree(){
   }
-  dual_invertible_binary_indexed_tree.cpp(int N, function<T(T, T)> f, function<T(T)> inv, T E): N(N), BIT(N + 1, E), f(f), inv(inv), E(E){
+  dual_invertible_binary_indexed_tree(int N, function<T(T, T)> f, function<T(T)> inv, T E): N(N), BIT(N + 1, E), f(f), inv(inv), E(E){
   }
-  dual_invertible_binary_indexed_tree.cpp(vector<T> &A, function<T(T, T)> f, function<T(T)> inv, T E): N(A.size()), BIT(N + 1), f(f), inv(inv), E(E){
+  dual_invertible_binary_indexed_tree(vector<T> &A, function<T(T, T)> f, function<T(T)> inv, T E): N(A.size()), BIT(N + 1), f(f), inv(inv), E(E){
     for (int i = 0; i < N; i++){
       BIT[i + 1] = A[i];
     }
