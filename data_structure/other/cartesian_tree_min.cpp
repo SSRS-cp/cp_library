1b2fbcdba8b18308234e265dca143676a60da9d4
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-15 08:34:17 +0900

    Create cartesian_tree_min.cpp

---
+++ data_structure/other/cartesian_tree_min.cpp
@@ -0,0 +1,31 @@
+vector<int> cartesian_tree_min(vector<int> &A){
+  int N = A.size();
+  vector<int> pr(N, -1);
+  stack<int> st;
+  st.push(0);
+  for (int i = 1; i < N; i++){
+    int prev = -1;
+    while (!st.empty()){
+      int j = st.top();
+      if (A[i] < A[j]){
+        st.pop();
+        if (prev != -1){
+          pr[prev] = j;
+        }
+        prev = j;
+      } else {
+        break;
+      }
+    }
+    if (prev != -1){
+      pr[prev] = i;
+    }
+    st.push(i);
+  }
+  while (st.size() >= 2){
+    int x = st.top();
+    st.pop();
+    pr[x] = st.top();
+  }
+  return pr;
+}
