3a0ef20ed931c6d7dfd082fb3c11d5403d5034a6
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-15 08:40:01 +0900

    Create queue_operate_all_composite.test.cpp

---
+++ test/library_checker/queue_operate_all_composite.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/queue_operate_all_composite"
+#include <bits/stdc++.h>
+using namespace std;
+const long long MOD = 998244353;
+#include "../../data_structure/other/sliding_window_aggregation.cpp"
+#include "../../other/monoids/linear.cpp"
+int main(){
+  int Q;
+  cin >> Q;
+  swag<linear> S(composite, linear());
+  for (int i = 0; i < Q; i++){
+    int t;
+    cin >> t;
+    if (t == 0){
+      int a, b;
+      cin >> a >> b;
+      S.push(linear(a, b));
+    }
+    if (t == 1){
+      S.pop();
+    }
+    if (t == 2){
+      int x;
+      cin >> x;
+      cout << value(S.get(),x) << endl;
+    }
+  }
+}
