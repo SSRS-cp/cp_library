676e4392e03564448eb68903f406306f43a396cf
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-14 12:57:56 +0900

    Create dsl_2_i.test.cpp

---
+++ test/aoj/dsl_2_i.test.cpp
@@ -0,0 +1,53 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_I"
+#include <bits/stdc++.h>
+using namespace std;
+#include "../../data_structure/sequence/lazy_segment_tree.cpp"
+struct monoid{
+  int cnt;
+  long long sum;
+  monoid(): cnt(0), sum(0){
+  }
+};
+int main(){
+  int n, q;
+  cin >> n >> q;
+  vector<monoid> A(n);
+  for (int i = 0; i < n; i++){
+    A[i].cnt = 1;
+  }
+  function<monoid(monoid, monoid)> op = [](monoid a, monoid b){
+    a.cnt += b.cnt;
+    a.sum += b.sum;
+    return a;
+  };
+  function<monoid(int, monoid)> mp = [](int a, monoid b){
+    if (a != -1){
+      b.sum = (long long) a * b.cnt;
+    }
+    return b;
+  };
+  function<int(int, int)> comp = [](int a, int b){
+    if (b == -1){
+      return a;
+    } else {
+      return b;
+    }
+  };
+  lazy_segment_tree<monoid, int> ST(A, op, mp, comp, monoid(), -1);
+  for (int i = 0; i < q; i++){
+    int c;
+    cin >> c;
+    if (c == 0){
+      int s, t, x;
+      cin >> s >> t >> x;
+      s--;
+      ST.range_apply(s, t, x);
+    }
+    if (c == 1){
+      int s, t;
+      cin >> s >> t;
+      s--;
+      cout << ST.range_fold(s, t).sum << endl;
+    }
+  }
+}
