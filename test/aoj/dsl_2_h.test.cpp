40fbcfe0f6b715230a1e568260ebec7595c4f7a4
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-14 10:01:56 +0900

    Update dsl_2_h.test.cpp

--- test/aoj/dsl_2_h.test.cpp
+++ test/aoj/dsl_2_h.test.cpp
@@ -1,23 +1,24 @@
 #define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_H"
 #include <bits/stdc++.h>
 using namespace std;
+const int INF = 1000000000;
 #include "../../data_structure/sequence/lazy_segment_tree.cpp"
 int main(){
   int n, q;
   cin >> n >> q;
   vector<int> a(n, 0);
-  function<int(int, int)> op = [](int a, int b){return min(a, b)};
+  function<int(int, int)> op = [](int a, int b){return min(a, b);};
   lazy_segment_tree<int, int> ST(a, op, plus<int>(), plus<int>(), INF, 0);
   for (int i = 0; i < q; i++){
     int c;
     cin >> c;
-    if (t == 0){
+    if (c == 0){
       int s, t, x;
       cin >> s >> t >> x;
       t++;
       ST.range_apply(s, t, x);
     }
-    if (t == 1){
+    if (c == 1){
       int s, t;
       cin >> s >> t;
       t++;
