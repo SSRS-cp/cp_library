c6b3e5b2e427fd4060f3617f9932db73a3f290db
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-16 04:32:52 +0900

    Create compact_bit_vector.cpp

---
+++ data_structure/sequence/compact_bit_vector.cpp
@@ -0,0 +1,33 @@
+struct compact_bit_vector{
+  vector<unsigned long long> A;
+  vector<int> S;
+  compact_bit_vector(vector<bool> &a){
+    int N = a.size();
+    int M = (N + 64 - 1) >> 6;
+    A = vector<unsigned long long>(M, 0);
+    for (int i = 0; i < M; i++){
+      for (int j = i << 6; j < min((i + 1) << 6, N); j++){
+        if (a[j]){
+          A[i] |= (unsigned long long) 1 << (j - (i << 6));
+        }
+      }
+    }
+    S = vector<int>(M + 1, 0);
+    for (int i = 0; i < M; i++){
+      S[i + 1] = S[i] + __builtin_popcountll(A[i]);
+    }
+  }
+  int rank(int k, int d){
+    int ans;
+    if ((k & 63) == 0){
+      ans = S[k >> 6];
+    } else {
+      ans = S[k >> 6] + __builtin_popcountll(A[k >> 6] << (64 - k + (k >> 6 << 6)));
+    }
+    if (d == 0){
+      return k - ans;
+    } else {
+      return ans;
+    }
+  }
+};
