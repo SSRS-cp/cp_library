c4de00dff3282f13d327758b3596dd8f1cf8016e
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-16 20:05:19 +0900

    Create itp1_6_a.test.cpp

---
+++ test/aoj/itp1_6_a.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_6_A"
+#include <bits/stdc++.h>
+using namespace std;
+#include "../../data_structure/sequence/compact_bit_vector.cpp"
+#include "../../data_structure/sequence/wavelet_matrix.cpp"
+int main(){
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++){
+    cin >> a[i];
+  }
+  wavelet_matrix WM(a);
+  for (int i = n - 1; i >= 0; i--){
+    cout << WM[i];
+    if (i > 0){
+      cout << ' ';
+    }
+  }
+  cout << endl;
+}
