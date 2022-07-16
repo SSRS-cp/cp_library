778675e54388bb65043fa3c400fdd63e295b0413
SSRS-cp <61640869+SSRS-cp@users.noreply.github.com>
2022-07-17 03:32:27 +0900

    Update static_range_sum.test.cpp

--- test/library_checker/static_range_sum.test.cpp
+++ test/library_checker/static_range_sum.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/static_range_sum"
 #include <bits/stdc++.h>
 using namespace std;
-#include "../../data_structure/sequence/cumulative_sum.cpp"
+#include "../../data_structure/sequence/cumulative_sum.hpp"
 int main(){
   int N, Q;
   cin >> N >> Q;
